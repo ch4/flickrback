8c3a2c07368baf7c546395a35171db0e2571e798
Harvey Chan <ch4ch4@gmail.com>
2013-09-28 19:35:58 -0700

    added library to generate html5 canvas image from html. bot working yet because the js pdf creator only supports jpeg

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-alert("test");
+// var canvas = document.documentElement;
+// var dataUrl = canvas.toDataURL();
+// alert(dataUrl);
+
+html2canvas(document.body, {
+  onrendered: function(canvas) {
+    //document.body.appendChild(canvas);
+	var dataUrl = canvas.toDataURL();
+	//alert(dataUrl);
+	
+	var doc = new jsPDF();
+	// doc.setFontSize(40);
+	// doc.text(35, 25, "test");
+	doc.addImage(dataUrl, 'PNG', 15, 40, 180, 160);
+	
+	// doc.fromHTML($('body').get(0), 15, 15, {
+	// 'width': 170, 
+	// 'elementHandlers': specialElementHandlers
+	// });
+	
+	doc.save('screenshot.pdf');
+  }
+});
+
