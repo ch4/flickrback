a4c95f62b4556b35d0c250b669ea4af307ba5d0b
Harvey Chan <ch4ch4@gmail.com>
2013-09-28 19:56:25 -0700

    added jquery. partially working, having trouble saving

--- src/main.js
+++ src/main.js
@@ -2,21 +2,38 @@
 // var dataUrl = canvas.toDataURL();
 // alert(dataUrl);
 
+var specialElementHandlers = {
+	'#editor': function(element, renderer){
+		return true;
+	}
+};
+
 html2canvas(document.body, {
   onrendered: function(canvas) {
-    //document.body.appendChild(canvas);
+    // document.body.appendChild(canvas);
 	var dataUrl = canvas.toDataURL();
 	//alert(dataUrl);
+	
+	//Create temporary div to insert the canvas element for printing to pdf
+	var div = document.createElement("div");
+	div.style.width = "100px";
+	div.style.height = "100px";
+	div.style.background = "red";
+	div.style.color = "white";
+	div.setAttribute('class','gitbackmachine');
+	div.appendChild(canvas);
+	document.body.appendChild(div);
+	
 	
 	var doc = new jsPDF();
 	// doc.setFontSize(40);
 	// doc.text(35, 25, "test");
-	doc.addImage(dataUrl, 'PNG', 15, 40, 180, 160);
+	//doc.addImage(dataUrl, 'JPEG', 15, 40, 180, 160);
 	
-	// doc.fromHTML($('body').get(0), 15, 15, {
-	// 'width': 170, 
-	// 'elementHandlers': specialElementHandlers
-	// });
+	doc.fromHTML($('.gitbackmachine').get(0), 15, 15, {
+	'width': 170, 
+	'elementHandlers': specialElementHandlers
+	});
 	
 	doc.save('screenshot.pdf');
   }
