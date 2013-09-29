625a618df126f5c40239607504e553db0b693380
Harvey Chan <ch4ch4@gmail.com>
2013-09-29 10:49:03 -0700

    added flickr keyfile

---
+++ src/keys.js
@@ -0,0 +1,14 @@
+
+//flickr oauth key
+var params = {
+    description: "desc1",
+    oauth_consumer_key : "xxxxx_your_consumer_key_xxxxx",
+    oauth_nonce : "77e66489c35f97cc54a3ca20a88149f1efe906da",
+    oauth_signature : "quWKoqMsaxCRKRB7yAVc8gSv0yU=",
+    oauth_signature_method : "HMAC-SHA1",
+    oauth_timestamp : "1364002382",
+    oauth_token: "xxxx_your_oauth_token_xxxxx",
+    oauth_version: "1.0",
+    tags: "tags1",
+    title: "title1"
+};
